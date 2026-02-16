87daa28ad56e19518d2b8f219fb27de14c908b46
kimia yousefi <94684507+kimia-yousefi@users.noreply.github.com>
2026-02-16 13:04:07 +0330

    Update couriershipment.sql

--- T-SQL - Transactions/couriershipment.sql
+++ T-SQL - Transactions/couriershipment.sql
@@ -9,14 +9,18 @@ WHERE ShipmentId = @ShipmentId;
 
 save tran upshipment
 
-IF not exists(
-    select 1 from courier where isactive = 1
+IF NOT EXISTS(
+    SELECT 1 
+    FROM Courier 
+    WHERE CourierId = @CourierId AND IsActive = 1
 )
 BEGIN
+    PRINT 'Courier is not active'
     ROLLBACK TRAN
 END
 ELSE
 BEGIN
+    PRINT 'Shipment not found'
     IF @@ROWCOUNT = 0
     BEGIN
         ROLLBACK TRAN upshipment
