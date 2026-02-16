bff82f4d73e6ca68c35514bd190b3382120fad54
kimia yousefi <94684507+kimia-yousefi@users.noreply.github.com>
2026-02-16 08:48:40 +0330

    Create OrderStatusUpdate.sql

---
+++ T-SQL - Transactions/OrderStatusUpdate.sql
@@ -0,0 +1,33 @@
+DECLARE @orderId int = 2
+DECLARE @NStatus int = 22
+
+BEGIN TRAN;
+
+UPDATE orders 
+SET status = @NStatus
+WHERE orderId = @orderId 
+
+SAVE TRAN changestatus
+
+IF not EXISTS (
+    SELECT 1 from customerinfo 
+    WHERE customerId = (
+        select customerId 
+        from orders 
+        where orderid = @orderId)
+)
+BEGIN
+    PRINT "customer is not active"
+    ROLLBACK TRAN
+END
+ELSE
+BEGIN
+    IF @@ROWCOUNT = 0
+    BEGIN
+    ROLLBACK TRAN changestatus
+    END
+    ELSE
+    BEGIN
+        COMMIT
+    END
+END
